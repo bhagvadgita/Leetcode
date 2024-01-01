57451827b011f0f5ba35acf9662b5d585f7e3cee
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-01-01 11:56:10 +0530

    Time: 410 ms (5.02%) | Memory: 18 MB (15.57%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int count=0;
+        for (int i=0; i<g.size(); i++){
+            for (int j=0; j<s.size();j++){
+                if (g[i]<=s[j]){
+                    count++;
+                    s.erase(s.begin()+j);
+                    break;
+                }
+            }
+        }
+        return count;
+        
+    }
+};

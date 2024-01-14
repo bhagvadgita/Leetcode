03470a48cfa21a48dd15335f777b1185763bf1a5
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-01-14 15:11:10 +0530

    Time: 11 ms (91.70%) | Memory: 18.8 MB (86.44%) - LeetSync

---
+++ 913-random-flip-matrix/random-flip-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int i=0;
+    int n;
+    int m;
+    int j=0;
+    Solution(int m, int n) {
+        this->n=n;
+        this->m=m;
+    }
+    
+    vector<int> flip() {
+        int I=i;
+        int J=j;
+        if (j+1<n){
+            j++;
+        }
+        else if (i+1<m){
+            i++;
+            j=0;
+        }
+        else{
+            i=0;
+            j=0;
+        }
+        return {I,J};
+    }
+    
+    void reset() {
+        
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(m, n);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */

9e7186dc56293a20b8246d450a0236bb0588e1e7
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-01-18 23:01:43 +0530

    Time: 10 ms (38.04%) | Memory: 6.5 MB (52.93%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector<int> arr(n);
+        for (int i=0; i<n; i++){
+            arr[i]=i+1;
+        }
+        int i=0;
+        int count=1;
+        while (arr.size()!=1){
+            if (count==k){
+                arr.erase(arr.begin()+i);
+                count=1;
+                
+            }
+            else{
+                count++;
+                i++;
+            }
+            if (i>=arr.size()){
+                i=0;
+            }
+        }
+        return arr[0];
+    }
+};

41d4e7efbd0603b009a7e5c444c5c89f416ac442
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-01-22 19:15:47 +0530

    Time: 2 ms (81.90%) | Memory: 7.9 MB (5.03%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0, cows = 0;
+        vector<int> numbers(10, 0);
+        for (int i = 0; i < secret.size(); i++) {
+            if (secret[i] == guess[i]) bulls++;
+            else {
+                if (numbers[secret[i]-'0']++ < 0) cows++;
+                if (numbers[guess[i]-'0']-- > 0) cows++;
+            }
+        }
+        return to_string(bulls) + "A" + to_string(cows) + "B";
+    }
+};

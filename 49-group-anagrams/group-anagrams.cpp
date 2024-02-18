3bc51100eda4880c7b6eb868c9a8da28d18aa9d5
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-02-18 14:00:50 +0530

    Time: 29 ms (54.37%) | Memory: 23.8 MB (57.85%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,27 +1,15 @@
 
 class Solution {
 public:
-    string getSignature(const string& s) {
-        vector<int> count(26, 0);
-        for (char c : s) {
-            count[c - 'a']++;
-        }
-
-        stringstream ss;
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                ss << (char)('a' + i) << count[i];
-            }
-        }
-        return ss.str();
-    }
 
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
         unordered_map<string, vector<string>> groups;
 
-        for (const string& s : strs) {
-            groups[getSignature(s)].push_back(s);
+        for (string s : strs) {
+            string t = s;
+            sort(s.begin(),s.end());
+            groups[s].push_back(t);
         }
 
         for (const auto& entry : groups) {

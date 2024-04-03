199cbb9aa5f371867f3a8f6fce1e9185cd08db6f
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-04-03 12:29:41 +0530

    Time: 30 ms (23.73%) | Memory: 22.4 MB (22.44%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -11,31 +11,22 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        stack<ListNode*> st;
+        if (!head || !head->next) return;
+        vector<ListNode*> vec;
         ListNode* curr = head;
         while (curr){
-            st.push(curr);
+            vec.push_back(curr);
             curr = curr->next;
         }
-        curr = head;
-        unordered_map<ListNode*,bool> vis;
-        while (curr){
-            ListNode* last = st.top();
-            st.pop();
-            ListNode* Next = curr->next;
-            if (vis[last]){
-                curr->next = NULL;
-                break;
-            }
-            curr->next = last;
-            vis[last] = true;
-            curr = curr->next;
-            if (vis[Next]){
-                curr->next = NULL;
-                break;
-            }
-            curr->next = Next;
-            curr = curr->next;
+        int i = 0;
+        int j = vec.size()-1;
+        while (i<j){
+            vec[i]->next = vec[j];
+            i++;
+            if (i==j) break;
+            vec[j]->next = vec[i];
+            j--;
         }
+        vec[i]->next = nullptr;
     }
 };

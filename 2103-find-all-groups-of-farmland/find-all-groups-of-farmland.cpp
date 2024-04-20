c8fdba76c97e8423c156b1a6ff27b0f56634d14d
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-04-21 00:31:44 +0530

    Time: 101 ms (72.00%) | Memory: 63.7 MB (77.82%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,53 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        int rows = land.size();
-        int cols = land[0].size();
-        unordered_set<string> visited;
-        vector<vector<int>> result;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (land[i][j] == 1 && visited.find(to_string(i) + "," + to_string(j)) == visited.end()) {
-                    vector<int> bounds = dfs(land, visited, i, j);
-                    result.push_back(bounds);
+        int n=land.size(), m=land[0].size();
+        vector<vector<int>> vis(n, vector<int>(m)), ans;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(vis[i][j]==1 || land[i][j]==0)continue;
+                int a=i, b=j,c=i,d=j;
+                while(d<m && land[a][d]==1)d++;
+                d--;
+                while(c<n && land[c][b]==1)c++;
+                c--;
+                ans.push_back({a,b,c,d});
+                for(int x=a; x<=c; x++){
+                    for(int y=b; y<=d; y++)vis[x][y]=1;
                 }
+                j=d;
             }
         }
-
-        return result;
-    }
-
-    vector<int> dfs(vector<vector<int>>& land, unordered_set<string>& visited, int x, int y) {
-        stack<pair<int, int>> stack;
-        stack.push({x, y});
-        visited.insert(to_string(x) + "," + to_string(y));
-
-        int minRow = x, minCol = y;
-        int maxRow = x, maxCol = y;
-
-        while (!stack.empty()) {
-            auto current = stack.top();
-            stack.pop();
-            int curX = current.first, curY = current.second;
-
-            vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-            for (auto& dir : directions) {
-                int nx = curX + dir.first;
-                int ny = curY + dir.second;
-
-                if (nx >= 0 && nx < land.size() && ny >= 0 && ny < land[0].size() &&
-                    land[nx][ny] == 1 && visited.find(to_string(nx) + "," + to_string(ny)) == visited.end()) {
-                    visited.insert(to_string(nx) + "," + to_string(ny));
-                    stack.push({nx, ny});
-                    minRow = min(minRow, nx);
-                    minCol = min(minCol, ny);
-                    maxRow = max(maxRow, nx);
-                    maxCol = max(maxCol, ny);
-                }
-            }
-        }
-
-        return {minRow, minCol, maxRow, maxCol};
+        return ans;
     }
 };

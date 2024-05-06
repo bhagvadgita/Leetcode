294c19084f34a2e5aac680a40c00bd4f12e72cc2
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-05-06 14:20:33 +0530

    Time: 270 ms (62.72%) | Memory: 164.4 MB (72.58%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -11,31 +11,15 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        while (curr){
-            ListNode* Next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = Next;
+        if(head == NULL || head->next == NULL) {
+            return head;
         }
-        curr = prev;
-        while (curr->next){
-            if (curr->next->val<curr->val){
-                curr->next = curr->next->next;
-            }
-            else{
-                curr = curr->next;
-            }
+        ListNode* nextN = removeNodes(head->next);
+        if(nextN->val > head->val) {
+            return nextN;
         }
-        curr = prev;
-        prev = NULL;
-        while (curr){
-            ListNode* Next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = Next;
-        }
-        return prev;
+        head->next = nextN;
+        return head;
     }
+    
 };

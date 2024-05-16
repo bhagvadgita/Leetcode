6927893d28228bd87831716d3d25e61c44929207
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-05-16 17:59:33 +0530

    Time: 8 ms (82.42%) | Memory: 17.5 MB (24.21%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -15,11 +15,9 @@ public:
         if (root->val == 2){
             return evaluateTree(root->left) || evaluateTree(root->right);
         }
-        else if (root->val == 3){
+        if (root->val == 3){
             return evaluateTree(root->left) && evaluateTree(root->right);
         }
-        else{
-            return root->val;
-        }
+        return root->val;
     }
 };

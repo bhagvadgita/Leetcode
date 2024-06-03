485cf77a4c20b05a9257af561e4a091bbfcf113f
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-06-03 06:52:10 +0530

    Time: 22 ms (26.94%) | Memory: 11.9 MB (83.42%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -5,12 +5,9 @@ public:
         int j = 0;
         while (i<s.size() && j<t.size()){
             if (s[i]==t[j]){
-                i++;
                 j++;
             }
-            else{
-                i++;
-            }
+            i++;
         }
         return t.size()-j;
     }

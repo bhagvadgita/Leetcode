8e459cca375be2d3af435bd45a661dbad561ead8
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-06-17 17:33:53 +0530

    Time: 202 ms (83.90%) | Memory: 9.4 MB (97.18%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
+
+    bool check(vector<vector<char>>& board, string& word, int idx, int i, int j){
+        if (idx==word.size()) return true;
+        if (i<0 || j<0 || i==board.size() || j==board[0].size() || board[i][j]!=word[idx]) return false;
+        char temp = board[i][j];
+        board[i][j] = '#';
+        bool flag = check(board,word,idx+1,i+1,j) || check(board,word,idx+1,i-1,j) || check(board,word,idx+1,i,j+1) || check(board,word,idx+1,i,j-1);
+        board[i][j] = temp;
+        return flag;
+    }
+
     bool exist(vector<vector<char>>& board, string word) {
         
-        for (int i = 0;i<board.size(); i++){
+        for (int i = 0; i<board.size(); i++){
             for (int j = 0; j<board[0].size(); j++){
-                if (board[i][j] == word[0]){
-                    if (check(board,word,0,i,j)) return true;
+                if (check(board,word,0,i,j)){
+                    return true;
                 }
             }
         }
         return false;
-    }
-    bool check(vector<vector<char>>& board, string& word, int idx, int i, int j){
-        if (idx == word.size()) return true;
-        if (i<0 || j<0 || i>=board.size() || j>=board[0].size() || word[idx]!= board[i][j]) return false;
-        char temp = board[i][j];
-        board[i][j] = '#';
-        bool found = (check(board,word,idx+1,i+1,j) || check(board,word,idx+1,i-1,j) || check(board,word,idx+1,i,j+1) || check(board,word,idx+1,i,j-1));
-        board[i][j] = temp;
-        return found;
     }
 };

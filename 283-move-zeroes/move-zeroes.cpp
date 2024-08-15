a778db34ee914d9cdb16b2e76af0584a78c74ea1
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-08-15 23:54:51 +0530

    Time: 15 ms (67.97%) | Memory: 21.9 MB (20.89%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,12 +4,10 @@ public:
         int j = 0;
         for (int i = 0; i<nums.size(); i++){
             if (nums[i]!=0){
-                nums[j] = nums[i];
+                swap(nums[i],nums[j]);
                 j++;
             }
         }
-        for (j;j<nums.size(); j++){
-            nums[j] = 0;
-        }
+        
     }
 };

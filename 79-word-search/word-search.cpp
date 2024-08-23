e7c16ef714658ee9fd74fcba92a564c11a36e60f
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-08-23 23:43:45 +0530

    Time: 207 ms (84.55%) | Memory: 9.9 MB (84.62%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
 
-    bool check(vector<vector<char>>& board, string& word, int idx, int i, int j){
-        if (idx==word.size()) return true;
-        if (i<0 || j<0 || i==board.size() || j==board[0].size() || board[i][j]!=word[idx]) return false;
-        char temp = board[i][j];
-        board[i][j] = '#';
-        bool flag = check(board,word,idx+1,i+1,j) || check(board,word,idx+1,i-1,j) || check(board,word,idx+1,i,j+1) || check(board,word,idx+1,i,j-1);
-        board[i][j] = temp;
-        return flag;
+    bool check(vector<vector<char>>& board, string& word, int x, int y, int idx){
+        if (idx>=word.size()) return true;
+        if (x>=board.size() || x<0 || y<0 || y>=board[0].size() || board[x][y]!=word[idx]){
+            return false;
+        }
+        char temp = board[x][y];
+        board[x][y] = '#';
+        if (check(board,word,x+1,y,idx+1) || check(board,word,x-1,y,idx+1) || check(board,word,x,y+1,idx+1) || check(board,word,x,y-1,idx+1)){
+            board[x][y] = temp;
+            return true;
+        }
+        board[x][y] = temp;
+        return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        
         for (int i = 0; i<board.size(); i++){
             for (int j = 0; j<board[0].size(); j++){
-                if (check(board,word,0,i,j)){
+                if (board[i][j]==word[0] && check(board,word,i,j,0)){
                     return true;
                 }
             }

5d62b8c0819fa85acc597e4159ed043145cd0b70
Abhay Saxena <abhaysaxena3111@gmail.com>
2024-08-26 19:19:58 +0530

    Time: 7 ms (27.31%) | Memory: 11.3 MB (52.14%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,14 +3,9 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string ans = "";
         sort(strs.begin(),strs.end());
-        int n = strs.size()-1;
-        for (int i=0; i<strs[0].size();i++){
-            if (strs[0][i]==strs[n][i]){
-                ans+=strs[0][i];
-            }
-            else{
-                return ans;
-            }
+        for (int i = 0; i<strs[0].size(); i++){
+            if (strs[0][i]==strs[strs.size()-1][i]) ans += strs[0][i];
+            else break;
         }
         return ans;
     }

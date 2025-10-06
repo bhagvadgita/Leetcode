26071d7fcb3781f5e5e92bbaeb553bb6690eb160
Abhay Saxena <abhaysaxena3111@gmail.com>
2025-10-06 23:46:50 +0530

    Time: 28 ms (23.83%) | Memory: 26 MB (48.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -5,12 +5,8 @@ public:
         for (int i = 0; i<strs.size(); i++){
             string str = strs[i];
             sort(str.begin(),str.end());
-            if (ump.find(str)!=ump.end()){
-                ump[str].push_back(strs[i]);
-            }
-            else{
-                ump[str] = {strs[i]};
-            }
+            if (ump.find(str)!=ump.end()) ump[str].push_back(strs[i]);
+            else ump[str] = {strs[i]};
         }
         vector<vector<string>> ans;
         for (auto vec: ump){
